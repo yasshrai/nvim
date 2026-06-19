e4aea2495f6f54bf3b761345d9557f2e70fd584d
Yash Rai <yash2154rai@gmail.com>
2026-06-19 16:13:09 +0530

    add theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -10,4 +10,5 @@ return {
 	{ "iruzo/matrix-nvim" },
 	{ "nyoom-engineering/oxocarbon.nvim" },
 	{ "EdenEast/nightfox.nvim" },
+	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
 }
